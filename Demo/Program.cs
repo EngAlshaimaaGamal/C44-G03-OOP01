1dfc49401817dc1a3f7cbb66b3f6ab9161d6ad76
Microsoft <Microsoft@ALSHAIMAA>
2025-07-23 12:21:35 +0300

    Boxing , unboxing

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Boxing / UnBoxing
+
+            #region  boxig [value type => reference type or object ] safe
+
+            //int x = 44;
+            //object y = x; // boxing
+            //             // implacity 
+            //             // take copy from vale to box in heap
+
+
+
+
+
+            #endregion
+
+
+            #region Unboxing [reference => value type] unsafe
+            //object x = "Aliaa";
+            //int y =(int ) x; // unboxing
+            //                 //  explicity 
+            //                 // unsage in run time
+
+            //Console.WriteLine(y); //.InvalidCastException:
+
+
+            #endregion
+
+
+            #endregion
+
+
+
+
+
+
         }
     }
 }
