a141a7ef09b8fe7ac910555df3a56d501522d011
Microsoft <Microsoft@ALSHAIMAA>
2025-07-23 21:32:06 +0300

    Nullable value type

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -36,6 +36,73 @@
             #endregion
 
 
+            #region Nullable value type 
+
+            #region Example 1
+            //int x = 10;
+            //x = null; // invalid 
+
+            //int? y = 10;
+            //y = null;
+
+            //Nullable<int> x = 10;
+            //x = null; // valid  
+
+
+
+            #endregion
+
+            #region Example 2
+
+            //int x = 10;
+            //int? y = x;
+            //// implicit conversion from int to Nullable<int>    
+
+            //int? m = 10; //null;
+            //int n =(int ) m;  // explicit conversion from Nullable<int> to int  unsafe
+
+            //Console.WriteLine(n); // InvalidOperationException: Nullable object must have a value. if null ; 
+
+
+
+            //int? x = null;
+            //int y;
+            //if (x is not null)
+            //{
+            //    y = (int )x;
+            //}
+            //else
+            //    y= 0;
+
+
+            //if (x.HasValue)
+            //{
+            //     y = x.Value; // safe
+
+            //}
+            //else
+            //    y = 0;
+
+            //  y = x.HasValue ? x.Value : 0;
+
+            //y = x ?? 0; // null coalescing operator 
+
+            //y = x.GetValueOrDefault(); // safe  
+
+            //Console.WriteLine( y);
+            //Console.WriteLine(x);
+
+
+
+
+
+
+
+            #endregion
+
+
+            #endregion
+
 
 
 
